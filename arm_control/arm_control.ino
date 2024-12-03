fcf7b1eec1d61bfab0524649e9625ab3e144fb2c
AH-321 <a.haglund321@gmail.com>
2024-12-03 15:28:54 -0800

    Update arm_control.ino

--- arm_control/arm_control.ino
+++ arm_control/arm_control.ino
@@ -1,12 +1,15 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
+const int rs = 13, en = 12, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 // Pin assignments for servos
-const int basePin = 9;
-const int armSegment1Pin = 10;
-const int armSegment2Pin = 11;
-const int armSegment3Pin = 12;
-const int grabberPin = 13;
+const int basePin = 2;
+const int armSegment1Pin = 8;
+const int armSegment2Pin = 9;
+const int armSegment3Pin = 10;
+const int grabberPin = 3;
 
 // Create Servo objects
 Servo baseServo;
@@ -21,32 +24,66 @@ const int joystick1YPin = A1;
 const int joystick2XPin = A2;
 const int joystick2YPin = A3;
 
-// Servo position limits
+// Servo initial positions
 int baseServoPos = 90;
 int armSegment1Pos = 90;
 int armSegment2Pos = 90;
 int armSegment3Pos = 90;
-int grabberPos = 90;
+int grabberPos = 156;
 
 void setup() {
-  // Attach servos to pins
+  // Setup LCD
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+
+  // Setup servos
   baseServo.attach(basePin);
   armSegment1Servo.attach(armSegment1Pin);
   armSegment2Servo.attach(armSegment2Pin);
   armSegment3Servo.attach(armSegment3Pin);
   grabberServo.attach(grabberPin);
   
+  Serial.begin(9600);
+}
+
+void loop() {
+  // X axis 1
+  if(analogRead(joystick1XPin) <= 505) {
+  baseServoPos -= 5;
+  baseServoPos = constrain(baseServoPos, 0, 180);
+  baseServo.write(baseServoPos);
+  } 
+  else if(analogRead(joystick1XPin) >= 518) {
+  baseServoPos += 5;
+  baseServoPos = constrain(baseServoPos, 0, 180);
+  baseServo.write(baseServoPos);
+  }
+
+  // Y axis 1
+  if(analogRead(joystick1YPin) <= 505) {
+    armSegment1Pos -= 5;
+    armSegment1Pos = constrain(armSegment1Pos, 0, 180);
+    armSegment1Servo.write(armSegment1Pos);
+  }
+  else if(analogRead(joystick1YPin) >= 518) {
+    armSegment1Pos += 5;
+    armSegment1Pos = constrain(armSegment1Pos, 0, 180);
+    armSegment1Servo.write(armSegment1Pos);
+  }
+    
+  // X axis 2
+  if(analog)
+}
+
+void displayValues() {
+  
+}
+
+void initialize() { 
   // Set initial servo positions
   baseServo.write(baseServoPos);
   armSegment1Servo.write(armSegment1Pos);
   armSegment2Servo.write(armSegment2Pos);
   armSegment3Servo.write(armSegment3Pos);
   grabberServo.write(grabberPos);
-
-  // Initialize Serial monitor (optional for debugging)
-  Serial.begin(9600);
-}
-
-void loop() {
-    
 }
